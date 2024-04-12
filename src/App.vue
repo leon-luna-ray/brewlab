07dbf618a8b80ab966e44315299094c023b78b47
Ray Luna <leon.luna.ray@gmail.com>
2024-04-11 21:56:55 -0700

    add brew method selection

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <main>
-    <div class="container py-[2rem]">
-      <h1 class="text-center">Coffee Ratio Calculator</h1>
-      <span>Pour over (1:16)</span>
-      <div class="fields">
+    <div class="container py-[2rem] flex-col-2 items-center">
+      <div class="title">
+        <h1 class="text-center">Coffee Ratio Calculator</h1>
+      </div>
+      <div class="flex gap-[0.5rem] justify-center md:w-[50%] flex-wrap">
+        <button v-for="item in ratios" 
+                :class="['btn zoom-in-out', {'active': brewMethod.name === item.name }]" 
+                @click="brewMethod = item">
+          <span>{{ item.name }}</span>
+        </button>
+      </div>
+      <div class="fields flex justify-center gap-[4rem]">
         <div>
           <h2>Coffee</h2>
           <input type="number" v-model="grams" @input="updateWaterFromCoffee" /> g
@@ -18,11 +26,13 @@
 </template>
 
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, watch, onBeforeMount } from 'vue'
+import { ratios } from '@/lib/data'
 
 const grams = ref(0);
 const milliliters = ref(0);
 const ratio = ref(16);
+const brewMethod = ref(null);
 
 const updateWaterFromCoffee = () => {
   milliliters.value = grams.value * ratio.value;
@@ -34,4 +44,11 @@ const updateCoffeeFromWater = () => {
 
 watch(grams, updateWaterFromCoffee);
 watch(milliliters, updateCoffeeFromWater);
+watch(brewMethod, (value) => {
+  console.log(value);
+});
+
+onBeforeMount(() => {
+  brewMethod.value = ratios[6];
+});
 </script>
---
+++ src/lib/data.js
@@ -0,0 +1,11 @@
+export const ratios = [
+    { name: 'AeroPress', ratio: 15 },
+    { name: 'Arabic', ratio: 12 },
+    { name: 'Cold Brew', ratio: 8 },
+    { name: 'Drip', ratio: 16 },
+    { name: 'Espresso', ratio: 2 },
+    { name: 'French Press', ratio: 17 },
+    { name: 'Pour Over', ratio: 16 },
+    { name: 'Moka Pot', ratio: 10 },
+    { name: 'Turkish', ratio: 10 },
+]
