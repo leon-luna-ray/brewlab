310adf57d1176cf21d3ee4dedae7c889c553122d
Ray Luna <leon.luna.ray@gmail.com>
2025-08-02 15:40:43 -0700

    ratio widget

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,10 @@
 <template>
     <header>
-        <div class="container flex flex-col justify-center items-center py-[1.25rem]">
-            <h1 class="font-bungee">Brew Lab</h1>
+        <div class="container flex flex-col justify-center items-center py-[1rem]">
+            <h1 class="font-bungee text-shadow-2xs text-shadow-red">BrewLab</h1>
             <!-- <h2 class="h1">☕️</h2> -->
             <p class="label-text !tracking-[2.2px]">Coffee Ratio Calculator</p>
+            <h2 class="text-[2.5rem] leading-[100%]">☕️</h2>
         </div>
     </header>
 </template>
