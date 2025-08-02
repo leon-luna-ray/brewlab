39482baa3273c71f384c3494e0649a7d968edb28
Ray Luna <leon.luna.ray@gmail.com>
2025-08-02 13:58:10 -0700

    header styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
     <header>
         <div class="container flex flex-col justify-center items-center py-[1.25rem]">
-            <h1 class="font-bungee-shade border-red">Brew Lab</h1>
-            <p class="label-text">Coffee Ratio Calculator</p>
+            <h1 class="font-bungee">Brew Lab</h1>
+            <p class="label-text !text-[1rem] tracking-[2px]">Coffee Ratio Calculator</p>
         </div>
     </header>
 </template>
