13df695a9028e610d4497f370b225106adeb8f39
Ray Luna <leon.luna.ray@gmail.com>
2025-08-02 19:56:47 -0700

    input fields

--- src/components/RatioWidget.vue
+++ src/components/RatioWidget.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="widget">
     <h2 class="label-text">Brewing Method</h2>
+    <!-- brew method buttons -->
     <div class="grid grid-cols-2 md:grid-cols-4 gap-x-[0.4rem] gap-[0.5rem]">
       <button v-for="item in ratios"
         :class="['btn light-blue flex flex-col  gap-[0.25rem] justify-center items-center', { 'active': brewMethod.name === item.name }]"
@@ -9,10 +10,39 @@
         <span class="font-[700] text-[0.75rem]">{{ item.name }}</span>
       </button>
     </div>
+    <!-- selected brew method -->
     <div class="border-btn bg-red text-white p-[0.5rem]">
       <div class="text-center">
         <p class="font-[700]">{{ brewMethod.name }}</p>
         <p class="text-[0.85rem]">Recomended Ratio 1:{{ brewMethod.ratio }}</p>
+      </div>
+    </div>
+    <!-- inputs -->
+    <div class="flex gap-[18px]">
+
+      <div class="input-field">
+        <label class="input-label">Coffee</label>
+        <div class="input-with-unit">
+          <input type="number" v-model="grams" @input="updateCoffeeFromWater" />
+          <span class="unit">g</span>
+        </div>
+      </div>
+
+      <div class="flex 
+        items-center justify-center pt-[20px]">
+
+        <div class="border-[2px] rounded-full h-[36px] w-[36px] flex items-center justify-center bg-light-green">
+          <IconSquare class="h-[12px] w-[12px]" />
+        </div>
+      </div>
+
+      <div class="input-field">
+        <label class="input-label">Water</label>
+        <div class="input-with-unit">
+          <input type="number" v-model="milliliters" @input="updateCoffeeFromWater" />
+          <span class="unit">ml</span>
+        </div>
+
       </div>
     </div>
     <!-- <div class="container py-8 flex-col-2 items-center">
