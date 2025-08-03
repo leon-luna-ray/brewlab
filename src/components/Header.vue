4ede9cf6e15b05bf1ed59670c92720ee9063ac06
Ray Luna <leon.luna.ray@gmail.com>
2025-08-02 19:24:31 -0700

    update page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,8 @@
 <template>
     <header>
-        <div class="container flex flex-col justify-center items-center py-[1rem]">
-            <h1 class="font-bungee text-shadow-2xs text-shadow-red">BrewLab</h1>
-            <!-- <h2 class="h1">☕️</h2> -->
-            <p class="label-text !tracking-[2.2px]">Coffee Ratio Calculator</p>
-            <h2 class="text-[2.5rem] leading-[100%]">☕️</h2>
+        <div class="container flex flex-col justify-center items-center py-[1.5rem]">
+            <h1 class="font-jockey-one uppercase">BrewLab</h1>
+            <p class="label-text !tracking-[1.2px]">Coffee Ratio Calculator</p>
         </div>
     </header>
 </template>
